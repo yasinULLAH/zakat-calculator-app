dac206e1652740bf6d1be7ca82078c5944e05805
yasinULLAH <115789093+yasinULLAH@users.noreply.github.com>
2025-07-15 16:29:00 +0500

    updated

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = 'zakat-manager-cache-v1';
+const CACHE_NAME = 'zakat-manager-v1';
 const urlsToCache = [
   './',
-  './zakatCalc.html',
+  './index.html',
   './manifest.json',
-  './prices-history.json'
+  './favicon.ico',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
+  'https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu:wght@400;700&display=swap',
+  'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -20,11 +24,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+        return response || fetch(event.request);
+      })
   );
 });
